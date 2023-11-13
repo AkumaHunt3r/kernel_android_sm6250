d46e947eed2562466ca4a05a6c71e56840b587f2
AkumaHunt3r <AkumaHunt3r@protonmail.com>
2023-11-13 03:30:47 +0000

    Revert "sched/fair: Improve the scheduler"
    This reverts commit ece0f3d36df025c2a4b899ec4c8854b8dfdf0bcc.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10112,8 +10112,7 @@ group_similar_cpu_capacity(struct sched_group *sg, struct sched_group *ref)
 	long diff = sg->sgc->min_capacity - ref->sgc->min_capacity;
 	long max = max(sg->sgc->min_capacity, ref->sgc->min_capacity);
 
-	return ((abs(diff) < max >> 3) ||
-		asym_cap_siblings(group_first_cpu(sg), group_first_cpu(ref)));
+	return abs(diff) < max >> 3;
 }
 
 static inline enum
