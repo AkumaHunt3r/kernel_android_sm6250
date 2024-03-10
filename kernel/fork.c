fc67d718b6c148d1434836f48e2def85aac96736
AkumaHunt3r <AkumaHunt3r@protonmail.com>
2024-03-10 00:39:43 +0000

    kernel: Boost CPU during app launch as well

--- kernel/fork.c
+++ kernel/fork.c
@@ -94,6 +94,7 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 #include <linux/simple_lmk.h>
 
@@ -2234,10 +2235,11 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
+	/* Boost DDR bus and CPU to the max for 50 ms when userspace launches an app. */
 	if (task_is_zygote(current) && df_boost_within_input(1000)) {
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_BW, 50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+		cpu_input_boost_kick_max(50);
 	}
 
 	/*
