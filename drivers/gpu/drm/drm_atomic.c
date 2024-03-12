cc5827dda3a35df37c623b2364b8323c8266b8d1
AkumaHunt3r <AkumaHunt3r@protonmail.com>
2024-03-12 22:34:47 +0000

    drm: Added KProfiles to rendering boosting

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -31,6 +31,8 @@
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/kprofiles.h>
 #include <linux/sync_file.h>
 
 #include "drm_crtc_internal.h"
@@ -2262,8 +2264,17 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 		(df_boost_within_input(3250))) {
-		devfreq_boost_kick(DEVFREQ_CPU_LLCC_BW);
-		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+		switch (kp_active_mode()) {
+		case 2:
+			devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_BW, 60);
+			devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 60);
+			break;
+		case 3:
+			cpu_input_boost_kick_max(60);
+			devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_BW, 120);
+			devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 120);
+			break;
+		}
 	}
 
 	drm_modeset_acquire_init(&ctx, 0);
