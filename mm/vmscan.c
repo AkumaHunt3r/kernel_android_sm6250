c5cb5e585d06b426d4fa46d60daf794a6334253a
AkumaHunt3r <AkumaHunt3r@protonmail.com>
2024-03-13 20:52:38 +0000

    mm: Mark kswapd and kshrinkd with the perf/big cores mask

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -6235,7 +6235,7 @@ int kswapd_run(int nid)
 	if (pgdat->kswapd)
 		return 0;
 
-	pgdat->kshrinkd = kthread_run_perf_critical(cpu_lp_mask, kshrinkd,
+	pgdat->kshrinkd = kthread_run_perf_critical(cpu_perf_mask, kshrinkd,
 								pgdat, "kshrinkd%d", nid);
 	if (IS_ERR(pgdat->kshrinkd)) {
 		/* failure at boot is fatal */
@@ -6246,7 +6246,7 @@ int kswapd_run(int nid)
 		return ret;
 	}
 
-	pgdat->kswapd = kthread_run_perf_critical(cpu_lp_mask, kswapd,
+	pgdat->kswapd = kthread_run_perf_critical(cpu_perf_mask, kswapd,
 								pgdat, "kswapd%d", nid);
 	if (IS_ERR(pgdat->kswapd)) {
 		/* failure at boot is fatal */
